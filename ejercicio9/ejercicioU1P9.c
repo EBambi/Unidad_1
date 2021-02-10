6dc5bf0d80eeb033724fe1fd1bed3dc18aecf0dd
Bambi <esteban.gonzalezg@upb.edu.co>
2021-02-09 19:51:22 -0500

    prueba9

--- ejercicio9/ejercicioU1P9.c
+++ ejercicio9/ejercicioU1P9.c
@@ -4,10 +4,11 @@ int main(void)
 {
     int a;
     printf("enter a number: ");
-    while (scanf("%d", &a) != 1)
+    scanf("%d", &a);
+    /*while (scanf("%d", &a) != 1)
     {
         // input was not a number, ask again:
         printf("enter a number: ");
-    }
+    }*/
     printf("You entered %d.\n", a);
 }
