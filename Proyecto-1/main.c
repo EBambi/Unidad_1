3a8b9609b848f469b5125f5848afc79d2cf0a6f5
Bambi <esteban.gonzalezg@upb.edu.co>
2021-02-20 20:10:48 -0500

    Proyecto1Terminado

--- Proyecto-1/main.c
+++ Proyecto-1/main.c
@@ -28,46 +28,82 @@ void mkreg_cedula_nombre_semestre(int cedulaNR, char nombreNR[30], int semestreN
 
 void readreg_cedula(int cedulaBuscar);
 
+void exitD(char fileName[30]);
+
 int main(void){
 
-    char nombreBaseDatos[30], nombreArchivo[30], nombreNuevo[30];
-    int cedulaNuevo, semestreNuevo, cedulaBusc;
+    char nombreBaseDatos[30], nombreArchivo[30], nombreNuevo[30], comando[10], c;
+    int cedulaNuevo, semestreNuevo, cedulaBusc, dec=0;
 
-    printf("Ingresar el nombre de la base de datos:\n");
-    scanf("%s", nombreBaseDatos);
-    printf("Ingrese la cantidad de registros de la base de datos:\n");
-    scanf("%d", &numRegis);
-    mkdb_nombre_tamanno(nombreBaseDatos, numRegis);
-    printf("La B.D. %s se creó correctamente.\n", nombreBaseDatos);
+    do
+    {
+        printf("-------Terminal-------\n");
+        printf("Ingrese un comando válido:\n");
+        scanf("%s",comando);
+        while ( (c = getchar()) != '\n' && c != EOF ) { }
+        if (strcmp(comando,"mkdb") == 0)
+        {
+            printf("Ingresar el nombre de la base de datos:\n");
+            scanf("%s", nombreBaseDatos);
+            printf("Ingrese la cantidad de registros de la base de datos:\n");
+            scanf("%d", &numRegis);
+            mkdb_nombre_tamanno(nombreBaseDatos, numRegis);
+            printf("La B.D. %s se creó correctamente.\n", nombreBaseDatos);
+        }
+        else if (strcmp(comando,"loaddb") == 0)
+        {
+            printf("Ingrese el nombre del archivo:\n");
+            scanf("%s", nombreArchivo);
+            loadbl_nombre(nombreArchivo);
+            printf("El archivo fue cargado correctamente\n");
+        }
+        else if (strcmp(comando,"savedb") == 0)
+        {
+            printf("Ingrese el nombre del archivo de salida:\n");
+            scanf("%s", nombreArchivo);
+            savedb_nombre(nombreArchivo);
+            printf("El archivo fue guardado correctamente\n");
+        }
+        else if (strcmp(comando,"readall") == 0)
+        {
+            printf("-----------------------\n");
+            readall(bufferDB);
+            printf("-----------------------\n");
+        }
+        else if (strcmp(comando,"readsize") == 0)
+        {
+            printf("%d\n",readSize(bufferDB));
+        }
+        else if (strcmp(comando,"mkreg") == 0)
+        {
+            printf("Ingrese la cédula del nuevo registro:\n");
+            scanf("%d",&cedulaNuevo);
+            printf("Ingrese el nombre del nuevo registro:\n");
+            scanf("%s",nombreNuevo);
+            printf("Ingrese el semestre del nuevo registro:\n");
+            scanf("%d",&semestreNuevo);
+            mkreg_cedula_nombre_semestre(cedulaNuevo, nombreNuevo, semestreNuevo);
+        }
+        else if (strcmp(comando,"readreg") == 0)
+        {
+            printf("Ingrese la cédula del estudiante que busca:\n");
+            scanf("%d",&cedulaBusc);
+            readreg_cedula(cedulaBusc);
+        }
+        else if (strcmp(comando,"exit") == 0)
+        {
+            printf("¿Desea guardar la base de datos antes de salir?\n(Sí = 1 --- No = 2)\n");
+            scanf("%d", &dec);
+            if (dec == 1)
+            {
+                printf("Ingrese el nombre del archivo de salida:\n");
+                scanf("%s", nombreArchivo);
+                savedb_nombre(nombreArchivo);            
+            }
+            else break;
+        }
 
-    printf("Ingrese el nombre del archivo:\n");
-    scanf("%s", nombreArchivo);
-    loadbl_nombre(nombreArchivo);
-
-    /*printf("Ingrese el nombre del archivo de salida:\n");
-    scanf("%s", nombreArchivo);
-    savedb_nombre(nombreArchivo);*/
-
-    printf("-----------------------\n");
-    readall(bufferDB);
-
-    /*printf("-----------------------\n");
-    printf("%d\n",readSize(bufferDB));*/
-
-    printf("Ingrese la cédula del nuevo registro:\n");
-    scanf("%d",&cedulaNuevo);
-    printf("Ingrese el nombre del nuevo registro:\n");
-    scanf("%s",nombreNuevo);
-    printf("Ingrese el semestre del nuevo registro:\n");
-    scanf("%d",&semestreNuevo);
-    mkreg_cedula_nombre_semestre(cedulaNuevo, nombreNuevo, semestreNuevo);
-
-    printf("-----------------------\n");
-    readall(bufferDB);
-
-    printf("Ingrese la cédula del estudiante que busca:\n");
-    scanf("%d",&cedulaBusc);
-    readreg_cedula(cedulaBusc);
+    } while (dec != 1 && dec != 2);
 
     return 0;
 }
@@ -138,7 +174,7 @@ int readSize(struct estudiante *buffer){
     {
         cont ++;
     }
-    return cont-1;
+    return cont;
 }
 
 void mkreg_cedula_nombre_semestre(int cedulaNR, char nombreNR[30], int semestreNR){
@@ -167,4 +203,10 @@ void readreg_cedula(int cedulaBuscar){
             printf("La cédula: %d pertenece a el estudiante %s. semestre: %d\n", (bufferDB+i)->cedula, (bufferDB+i)->nombre, (bufferDB+i)->semestre);
         }
     }
+}
+
+void exitD(char fileName[30]){
+
+    savedb_nombre(fileName);
+
 }
